e574cedf10f562490d86678585b0a8e23deaed65
mtalala <superraioi2014@gmail.com>
2026-02-27 15:20:48 -0300

    chore: use environment variables for configuration

--- src/main/java/br/projeto/nuvem/MissionController.java
+++ src/main/java/br/projeto/nuvem/MissionController.java
@@ -8,7 +8,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/mission")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "${app.cors.allowed-origins}")
 public class MissionController {
 
     @PostMapping
