487272acd0ac066a2032a411f1f31aa7c0c2c551
mtalala <superraioi2014@gmail.com>
2026-02-27 09:24:41 -0300

    Create Project

---
+++ src/test/java/br/projeto/nuvem/NuvemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.projeto.nuvem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuvemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
